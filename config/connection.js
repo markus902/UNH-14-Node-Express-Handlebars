cb15af4b45a1b9c4819eeb68893f91c49ccf27a2
Markus Markowich <markus902@gmx.de>
2019-10-05 21:05:43 -0400

    changed port for connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
+    port: 3306,
     user: "root",
     password: "Helmar@123",
     database: "burgers_db"
