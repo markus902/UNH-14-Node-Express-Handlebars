50e0cf1230a6009bfd7a885713149ff2ed70e59e
Markus Markowich <markus902@gmx.de>
2019-10-05 21:04:42 -0400

    working orm for select all

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,22 @@
 var connection = require("./connection.js");
+
+var orm = {
+    all: function (cb) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        });
+    },
+
+    newBurger: function (tableInput, colToSearch, valOfCol) {
+        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
+}
+
+module.exports = orm;
