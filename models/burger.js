87efc13f232721b7983c8594aa9aac52071c05f3
Markus Markowich <markus902@gmx.de>
2019-10-05 21:05:00 -0400

    working burger model

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,21 @@
 var orm = require("../config/orm.js");
+
+var burger = {
+    all: function (cb) {
+        orm.all(function (data) {
+            cb(data);
+        });
+    },
+    create: function (name, cb) {
+        orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
+    },
+    update: function (id, cb) {
+        var condition = "id=" + id;
+        orm.update("burgers", {
+                devoured: true
+            },
+            conditions, cb);
+    }
+};
+
+module.exports = burger
