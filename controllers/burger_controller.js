e4eebfe4a0cfef866a6334e108f48e4051c64118
Markus Markowich <markus902@gmx.de>
2019-10-05 21:06:12 -0400

    working burger controller for index route

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,2 +1,23 @@
-var express = require("express");
-var burger = require("../models/burgers.js")
+var router = require("express").Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        console.log(data);
+        let burgers = [];
+        data.forEach(element => {
+            burgers.push({
+                burger_name: element.burger_name,
+                devoured: element.devoured,
+                id: element.id
+            })
+        });
+        let hbsObject = {
+            burgers: burgers
+        }
+        res.render("index", hbsObject)
+
+    })
+});
+
+module.exports = router;
